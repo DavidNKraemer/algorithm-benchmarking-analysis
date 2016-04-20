d6fe5bd56ea93d1a7b0b32a6db84d96a552b149f
VBashlovkina <bashlovk@grinnell.edu>
2016-04-19 20:51:53 -0500

    add recursive inary search

---
+++ python_code/search/binary_search_recursive.py
@@ -0,0 +1,29 @@
+# add the parent directory to the path
+import sys
+sys.path.append('..')
+
+from util import isclose
+
+def binary_search(arr, x):
+    return binary_search_rec(arr, 0, len(arr), x)
+    
+def binary_search_rec(arr, lo, hi, x):
+    if (lo == hi):
+        return -1
+    else:
+        mid = (hi + lo) // 2
+        if (isclose(arr[mid], x)):
+            return mid
+        elif (arr[mid] > x): 
+            return binary_search_rec(arr, lo, mid, x)
+        else: # arr[mid] < x 
+            return binary_search_rec(arr, mid + 1, hi, x)
+
+if __name__ == '__main__':
+    arr = list(range(100))
+    for i in arr:
+        assert(i == binary_search(arr, i))
+    
+    assert(-1 == binary_search(arr, 200))
+    assert(-1 == binary_search(arr, -10))    
+    print("Success!")
