791b4597847ac1e4ebadc650841bfe64f4d01c0e
VBashlovkina <bashlovk@grinnell.edu>
2016-04-30 13:50:59 -0500

    add shuffling for testing

--- python_code/util.py
+++ python_code/util.py
@@ -1,6 +1,23 @@
+import random
+
 """
     Check if two floats are close enough.
     Adopted from math.isclose in Python 3.5
 """
 def isclose(a, b, rel_tol=1e-09):
     return abs(a-b) <= rel_tol * max(abs(a), abs(b))
+
+"""
+    Shuffle an array.
+    Returns a permutation of the original. Original array remains intact.
+"""
+def shuffle(arr):
+    l = len(arr)
+    ret = list(arr) # make a copy of the original
+    for i in range(l**2):
+        j = random.randint(0, l-1)
+        k = random.randint(0, l-1)
+        temp = ret[j]
+        ret[j] = ret[k]
+        ret[k] = temp
+    return ret
