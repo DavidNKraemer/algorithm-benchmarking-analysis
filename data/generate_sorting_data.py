d9e26f5ff8570cc49cd938c46c1fb3037226f196
VBashlovkina <bashlovk@grinnell.edu>
2016-05-01 23:14:39 -0500

    recover data generating scripts

---
+++ data/generate_sorting_data.py
@@ -0,0 +1,48 @@
+"""
+Generate a csv of unsorted uniformly random float arrays.
+
+Command line args: min_length(=10) max_length(=1000) incr(=50) repeats(=1) fn
+
+Default values only generate 20 arrays.
+"""
+from sys import argv
+import random
+
+
+"""
+Generate a sorted array of floats of specified size
+"""
+def generate_uniform_array(size):
+    arr = []
+    for i in range(size):
+        arr.append(random.uniform(0.0, 1000.0))
+    return arr
+
+"""
+Make a comma separated string from an array
+"""
+def array_to_csv_string(arr):
+    s = ''
+    for i in range(len(arr) - 1):
+        s += str(arr[i]) + ','
+    s += str(arr[len(arr) - 1])
+    return s
+
+if __name__ == '__main__':
+    min_length = int(argv[1]) if len(argv) > 1 else 10
+    max_length = int(argv[2]) if len(argv) > 2 else 1000
+    increments = int(argv[3]) if len(argv) > 3 else 50
+    repeats    = int(argv[4]) if len(argv) > 4 else 1
+    
+    fn = argv[5] if len(argv) > 5 else "sorting_data.csv"
+
+    current_size = min_length
+    with open(fn, 'w') as fp:
+        while(current_size < max_length):
+            for i in range(repeats):
+                # generate an array of current_size
+                arr = generate_unsorted_array(current_size)
+                # write the unsorted array
+                fp.write(array_to_csv_string(arr)+'\n')
+            current_size += increments
+            
