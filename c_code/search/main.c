4d5e37602dfcd9d7e912199a0dceaf6c05750981
kraemerd17 <kraemerd17@grinnell.edu>
2016-05-04 00:13:29 -0500

    The C code works now.

--- c_code/search/main.c
+++ c_code/search/main.c
@@ -2,7 +2,7 @@
 int main(int argc, char ** argv) {
     void (*search_ptr) = binary_search_rec;
     const char * input_source = "../../data/input_data/search_data.csv";
-    const char * output_source = "../../data/output_data/c_data/msr.csv";
+    const char * output_source = "../../data/output_data/c_data/bsr.csv";
     analyze_search_data(input_source, output_source, search_ptr);
     return 0;
 }
