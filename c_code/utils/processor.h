334b8e395e66840ff35ccee8d8a6937302c6fc30
David N. Kraemer <kraemerd17@grinnell.edu>
2016-05-04 20:01:29 -0500

    Renamed 'reader.*' to 'processor.*'

---
+++ c_code/utils/processor.h
@@ -0,0 +1,21 @@
+#ifndef PROCESSOR_H
+#define PROCESSOR_H
+
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+#include <time.h>
+#include <sys/time.h>
+
+int analyze_sort_data(const char * infile_name, 
+                      const char * outfile_name,
+                      void (*sort_pointer)(int *, int),
+                      const char * sort_name);
+
+int analyze_search_data(const char * infile_name, 
+                        const char * outfile_name,
+                        int (*search_pointer)(int *, int, int),
+                        const char * search_name);
+
+void print_array(int * array, int size);
+#endif
