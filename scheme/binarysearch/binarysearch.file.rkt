20c00848b12a37c054dac56bd3572c564383f4b9
Bo Wang <wangbo@grinnell.edu>
2016-05-05 11:24:55 -0500

    binary search code in scheme works

--- scheme/binarysearch/binarysearch.file.rkt
+++ scheme/binarysearch/binarysearch.file.rkt
@@ -25,15 +25,17 @@
     ;because of configuration
     (let kernel ([source (open-input-file input-file)]
                  [target (open-output-file output-file)])
-      (let ([nextval (read source)])
+      (let ([key (read source)]
+            [nextval (read source)])
         (cond 
           [(eof-object? nextval) 
            (close-input-port source)
            (close-output-port target)]
           [else 
-           (write (binary-search-time (cdr nextval) (car nextval)) target)
+           (write (binary-search-time nextval key) target)
            (display "," target)
            (write (length nextval) target)
+           (display "," target)
            (display "binary-search" target)
            (display "," target)
            (display "R" target)
